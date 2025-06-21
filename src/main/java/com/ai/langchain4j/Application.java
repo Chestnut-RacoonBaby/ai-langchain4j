95fb75121d2fe86fa317489d8d67dc4aff8ef8e7
qiaohaojie <qhj991015@163.com>
2025-06-21 16:35:11 +0800

    feat(langchain4j): 项目初始化

---
+++ src/main/java/com/ai/langchain4j/Application.java
@@ -0,0 +1,15 @@
+package com.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: qiaohaojie
+ * @date: 2025-06-21 16:22:35
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
