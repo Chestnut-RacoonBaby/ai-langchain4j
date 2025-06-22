49ae95531c8be0eca0cf4b2990d76460ab3fe50b
qiaohaojie <qhj991015@163.com>
2025-06-22 21:56:39 +0800

    feat(langchain4j): 启动类移动一下位置

--- src/main/java/com/ai/langchain4j/application/Application.java
+++ src/main/java/com/ai/langchain4j/application/Application.java
@@ -1,4 +1,4 @@
-package com.ai.langchain4j;
+package com.ai.langchain4j.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
