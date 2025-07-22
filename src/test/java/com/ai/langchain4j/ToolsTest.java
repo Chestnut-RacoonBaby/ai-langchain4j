fae09b27d5ca1e4b70aa76c7e16da97e70818baa
qiaohaojie <qhj991015@163.com>
2025-07-22 22:25:37 +0800

    feat(langchain4j): Tools工具测试类

---
+++ src/test/java/com/ai/langchain4j/ToolsTest.java
@@ -0,0 +1,25 @@
+package com.ai.langchain4j;
+
+import com.ai.langchain4j.assistant.SeparateChatAssistant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Function Calling函数，也叫tools工具
+ *
+ * @author: qiaohaojie
+ * @date: 2025-07-22 22:18:51
+ */
+@SpringBootTest
+public class ToolsTest {
+
+    @Autowired
+    private SeparateChatAssistant separateChatAssistant;
+
+    @Test
+    public void testCalculatorTools() {
+        String answer = separateChatAssistant.chat(8, "请帮我计算2+2等于几，顺便计算一下2334343的平方根是多少？");
+        System.out.println(answer);
+    }
+}
