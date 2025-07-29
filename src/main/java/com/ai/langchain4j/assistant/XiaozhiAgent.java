5985a5a227da4dc56ff63ef9945373b866e83e5c
qiaohaojie <qhj991015@163.com>
2025-07-29 22:46:02 +0800

    feat(langchain4j): 流式输出模型

--- src/main/java/com/ai/langchain4j/assistant/XiaozhiAgent.java
+++ src/main/java/com/ai/langchain4j/assistant/XiaozhiAgent.java
@@ -5,6 +5,7 @@ import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
+import reactor.core.publisher.Flux;
 
 /**
  * xiaozhi智能体
@@ -14,7 +15,8 @@ import dev.langchain4j.service.spring.AiServiceWiringMode;
  */
 @AiService(
         wiringMode = AiServiceWiringMode.EXPLICIT,
-        chatModel = "qwenChatModel",
+//        chatModel = "qwenChatModel",
+        streamingChatModel = "qwenStreamingChatModel",
         // 这个beanName是从配置类（XiaozhiAgentConfig）中获取的
         chatMemoryProvider = "chatMemoryProviderXiaozhi",
         // 配置工具
@@ -25,6 +27,20 @@ import dev.langchain4j.service.spring.AiServiceWiringMode;
 )
 public interface XiaozhiAgent {
 
-    @SystemMessage(fromResource = "xiaozhi-prompt-template.txt")
-    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+//    @SystemMessage(fromResource = "xiaozhi-prompt-template.txt")
+//    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+
+    /**
+     * 流式输出
+     * 大模型的流式输出是指大模型在生成文本或其他类型的数据时，不是等到整个生成过程完成后再一次性
+     * 返回所有内容，而是生成一部分就立即发送一部分给用户或下游系统，以逐步、逐块的方式返回结果。
+     * 这样，用户就不需要等待整个文本生成完成再看到结果。通过这种方式可以改善用户体验，因为用户不
+     * 需要等待太长时间，几乎可以立即开始阅读响应。
+     *
+     * @param memoryId    memoryId
+     * @param userMessage userMessage
+     * @return
+     */
+    @SystemMessage(fromResource = "zhaozhi-prompt-template.txt")
+    Flux<String> chat(@MemoryId Long memoryId, @UserMessage String userMessage);
 }
--- src/main/java/com/ai/langchain4j/controller/XiaozhiController.java
+++ src/main/java/com/ai/langchain4j/controller/XiaozhiController.java
@@ -2,12 +2,14 @@ package com.ai.langchain4j.controller;
 
 import com.ai.langchain4j.assistant.XiaozhiAgent;
 import com.ai.langchain4j.bean.ChatForm;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * @author: qiaohaojie
@@ -21,8 +23,14 @@ public class XiaozhiController {
     @Autowired
     private XiaozhiAgent xiaozhiAgent;
 
-    @PostMapping("/chat")
-    public String chat(@RequestBody ChatForm chatForm) {
+//    @PostMapping("/chat")
+//    public String chat(@RequestBody ChatForm chatForm) {
+//        return xiaozhiAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
+//    }
+
+    @Operation(summary = "对话")
+    @PostMapping(value = "/chat", produces = "text/stream;charset=utf-8")
+    public Flux<String> chat(@RequestBody ChatForm chatForm) {
         return xiaozhiAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
 }
